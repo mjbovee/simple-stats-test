4df1e6670bc9d7f550c11ae0c783063d359478cc
Michael Bovee <michael.j.bovee@gmail.com>
2019-02-06 14:22:40 -0700

    added tests to verify the data in the "edit" view was consistent

--- cypress/integration/firstTest.js
+++ cypress/integration/firstTest.js
@@ -16,91 +16,99 @@ describe( 'Testing Simple Statistics Block', function() {
         cy.get( '#wp-submit' ).click()
     } )
 
-    // it( 'Adds a simple stat block with value', function() {
-    //     cy.visit( baseURL + '/wp-admin/post-new.php?post_type=page' )
-    //     cy.get( '[aria-label="Disable tips"]' ).click()
-    //     cy.get( '#post-title-0' ).type( title1 ).blur()
-    //     cy.get( '[aria-label="Add block"]:first' ).click()
-    //     cy.get( '.editor-block-list-item-cgb-block-gutenberg-simple-statistics' ).click()
-    //     cy.get( '.wp-block-cgb-block-gutenberg-simple-statistics .components-button' ).click()
-    //     cy.get( '.statistic textarea.value').type(value1)
-    //     cy.get( '.statistic textarea.label').type(value2).blur()
-    //     cy.wait(1000)
-    //     cy.get( '.editor-post-publish-panel__toggle' ).click()
-    //     cy.wait(1000)
-    //     cy.get( '.editor-post-publish-button' ).click()
-    // } )
+    it( 'Adds a simple stat block with value', function() {
+        cy.visit( baseURL + '/wp-admin/post-new.php?post_type=page' )
+        cy.get( '[aria-label="Disable tips"]' ).click()
+        cy.get( '#post-title-0' ).type( title1 ).blur()
+        cy.get( '[aria-label="Add block"]:first' ).click()
+        cy.get( '.editor-block-list-item-cgb-block-gutenberg-simple-statistics' ).click()
+        cy.get( '.wp-block-cgb-block-gutenberg-simple-statistics .components-button' ).click()
+        cy.get( '.statistic textarea.value').type(value1)
+        cy.get( '.statistic textarea.label').type(value2).blur()
+        cy.wait(1000)
+        cy.get( '.editor-post-publish-panel__toggle' ).click()
+        cy.wait(1000)
+        cy.get( '.editor-post-publish-button' ).click()
+    } )
 
-    // it ( 'Adds a simple stat block without values', function() {
-    //     cy.visit( baseURL + '/wp-admin/post-new.php?post_type=page' )
-    //     cy.get( '[aria-label="Disable tips"]' ).click()
-    //     cy.get( '#post-title-0' ).type( title2 ).blur()
-    //     cy.get( '[aria-label="Add block"]:first' ).click()
-    //     cy.get( '.editor-block-list-item-cgb-block-gutenberg-simple-statistics' ).click()
-    //     cy.get( '.wp-block-cgb-block-gutenberg-simple-statistics .components-button' ).click()
-    //     cy.get( '.editor-post-publish-panel__toggle' ).click()
-    //     cy.wait(1000)
-    //     cy.get( '.editor-post-publish-button' ).click()
-    // } )
+    it ( 'Adds a simple stat block without values', function() {
+        cy.visit( baseURL + '/wp-admin/post-new.php?post_type=page' )
+        cy.get( '[aria-label="Disable tips"]' ).click()
+        cy.get( '#post-title-0' ).type( title2 ).blur()
+        cy.get( '[aria-label="Add block"]:first' ).click()
+        cy.get( '.editor-block-list-item-cgb-block-gutenberg-simple-statistics' ).click()
+        cy.get( '.wp-block-cgb-block-gutenberg-simple-statistics .components-button' ).click()
+        cy.get( '.editor-post-publish-panel__toggle' ).click()
+        cy.wait(1000)
+        cy.get( '.editor-post-publish-button' ).click()
+    } )
 
-    // it ( 'Adds a simple stat block with multiple values', function() {
-    //     cy.visit( baseURL + '/wp-admin/post-new.php?post_type=page' )
-    //     cy.get( '[aria-label="Disable tips"]' ).click()
-    //     cy.get( '#post-title-0' ).type( title3 ).blur()
-    //     cy.get( '[aria-label="Add block"]:first' ).click()
-    //     cy.get( '.editor-block-list-item-cgb-block-gutenberg-simple-statistics' ).click()
-    //     cy.get( '.wp-block-cgb-block-gutenberg-simple-statistics .components-button' ).click()
-    //     cy.get( '.statistic textarea.value').type(value1)
-    //     cy.get( '.statistic textarea.label').type(value2)
-    //     cy.get( '.wp-block-cgb-block-gutenberg-simple-statistics div:nth-child(2) .components-button.components-icon-button' ).click()
-    //     cy.get( '.statistic:nth-of-type(2) textarea.value').type(value3)
-    //     cy.get( '.statistic:nth-of-type(2) textarea.label').type(value4).blur()
-    //     cy.wait(1000)
-    //     cy.get( '.editor-post-publish-panel__toggle' ).click()
-    //     cy.wait(1000)
-    //     cy.get( '.editor-post-publish-button' ).click()
-    // } )
+    it ( 'Adds a simple stat block with multiple values', function() {
+        cy.visit( baseURL + '/wp-admin/post-new.php?post_type=page' )
+        cy.get( '[aria-label="Disable tips"]' ).click()
+        cy.get( '#post-title-0' ).type( title3 ).blur()
+        cy.get( '[aria-label="Add block"]:first' ).click()
+        cy.get( '.editor-block-list-item-cgb-block-gutenberg-simple-statistics' ).click()
+        cy.get( '.wp-block-cgb-block-gutenberg-simple-statistics .components-button' ).click()
+        cy.get( '.statistic textarea.value').type(value1)
+        cy.get( '.statistic textarea.label').type(value2)
+        cy.get( '.wp-block-cgb-block-gutenberg-simple-statistics div:nth-child(2) .components-button.components-icon-button' ).click()
+        cy.get( '.statistic:nth-of-type(2) textarea.value').type(value3)
+        cy.get( '.statistic:nth-of-type(2) textarea.label').type(value4).blur()
+        cy.wait(1000)
+        cy.get( '.editor-post-publish-panel__toggle' ).click()
+        cy.wait(1000)
+        cy.get( '.editor-post-publish-button' ).click()
+    } )
 
     it ( 'Tests that published values match input values - ' + title1, function() {
         cy.visit( baseURL + '/wp-admin/edit.php?post_type=page' )
         cy.get( '[aria-label="View “' + title1 + '”"]' ).click( {force: true} )
-        cy.get( '.simple-statistic-countup-counted' ).should( 'have.attr', 'data-value', value1 )
+        cy.get( '.simple-statistic-countup-counted' ).should( 'have.attr', 'data-value', value1 ).should( 'have.text', value1.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") )
         cy.get( '.label' ).should( 'have.text', value2 );
     } )
 
     it ( 'Tests that published values match input values - ' + title2, function() {
         cy.visit( baseURL + '/wp-admin/edit.php?post_type=page' )
         cy.get( '[aria-label="View “' + title2 + '”"]' ).click( {force: true} )
-        cy.get( '.simple-statistic-countup-counted' ).should( 'have.attr', 'data-value', '' ) 
+        cy.get( '.simple-statistic-countup-counted' ).should( 'have.attr', 'data-value', '' ).should( 'have.text', 'NaN' ) 
         cy.get( '.label' ).should( 'have.text', '' );
     } )
 
     it ( 'Tests that published values match input values - ' + title3, function() {
         cy.visit( baseURL + '/wp-admin/edit.php?post_type=page' )
         cy.get( '[aria-label="View “' + title3 + '”"]' ).click( {force: true} )
-        cy.get( '.statistic:nth-of-type(1) .simple-statistic-countup-counted' ).should( 'have.attr', 'data-value', value1 )
+        cy.get( '.statistic:nth-of-type(1) .simple-statistic-countup-counted' ).should( 'have.attr', 'data-value', value1 ).should( 'have.text', value1.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") )
         cy.get( '.statistic:nth-of-type(1) .label' ).should( 'have.text', value2 );
-        cy.get( '.statistic:nth-of-type(2) .simple-statistic-countup-counted' ).should( 'have.attr', 'data-value', value3 )
+        cy.get( '.statistic:nth-of-type(2) .simple-statistic-countup-counted' ).should( 'have.attr', 'data-value', value3 ).should( 'have.text', 'NaN' )
         cy.get( '.statistic:nth-of-type(2) .label' ).should( 'have.text', value4 );
     } )
 
-    // it ( 'Tests that blocks display correct values in edit view - ' + title1, function() {
-    //     cy.visit( baseURL + '/wp-admin/edit.php?post_type=page' )
-    //     cy.get( '[aria-label="“' + title1 + '” (Edit)"]' ).click()
-    //     cy.get( '[aria-label="Disable tips"]' ).click()
-    //     cy.get( '.simple-statistic-countup' ).should( 'have.attr', value1.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") )
-    //     cy.get( '.label' ).should( 'have.text', value2 );
-    // } )
+    it ( 'Tests that blocks display correct values in edit view - ' + title1, function() {
+        cy.visit( baseURL + '/wp-admin/edit.php?post_type=page' )
+        cy.get( '[aria-label="“' + title1 + '” (Edit)"]' ).click()
+        cy.get( '[aria-label="Disable tips"]' ).click()
+        cy.get( '.simple-statistic-countup' ).should( 'have.attr', 'data-value', value1 ).should( 'have.text', value1.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") )
+        cy.get( '.label' ).should( 'have.text', value2 );
+    } )
 
-    // it ( 'Tests that blocks display correct values in edit view - ' + title2, function() {
-    //     cy.visit( baseURL + '/wp-admin/edit.php?post_type=page' )
-    //     cy.get( '[aria-label="“' + title1 + '” (Edit)"]' ).click()
-    //     cy.get( '[aria-label="Disable tips"]' ).click()
-    //     cy.get( '.simple-statistic-countup' ).should( 'have.text', '0' ) 
-    //     cy.get( '.label' ).should( 'have.text', '' );
-    // } )
+    it ( 'Tests that blocks display correct values in edit view - ' + title2, function() {
+        cy.visit( baseURL + '/wp-admin/edit.php?post_type=page' )
+        cy.get( '[aria-label="“' + title2 + '” (Edit)"]' ).click()
+        cy.get( '[aria-label="Disable tips"]' ).click()
+        cy.get( '.simple-statistic-countup' ).should( 'have.attr', 'data-value', '' ).should( 'have.text', '0') 
+        cy.get( '.label' ).should( 'have.text', '' );
+    } )
 
-
+    it ( 'Tests that blocks display correct values in edit view - ' + title3, function() {
+        cy.visit( baseURL + '/wp-admin/edit.php?post_type=page' )
+        cy.get( '[aria-label="“' + title3 + '” (Edit)"]' ).click()
+        cy.get( '[aria-label="Disable tips"]' ).click()
+        cy.get( '.statistic:nth-of-type(1) .simple-statistic-countup' ).should( 'have.attr', 'data-value', value1 ).should( 'have.text', value1.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") )
+        cy.get( '.statistic:nth-of-type(1) .label' ).should( 'have.text', value2 );
+        cy.get( '.statistic:nth-of-type(2) .simple-statistic-countup' ).should( 'have.attr', 'data-value', value3 ).should( 'have.text', '0' )
+        cy.get( '.statistic:nth-of-type(2) .label' ).should( 'have.text', value4 );
+    } )
         
     // test that you can edit the data in the simple stat block
 
