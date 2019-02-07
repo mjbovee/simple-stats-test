ed5bcb2b23435ce6c31cc5521abd67fc3b385942
Michael Bovee <michael.j.bovee@gmail.com>
2019-02-07 13:02:15 -0700

    added two "edit" test blocks

--- cypress/integration/editData.js
+++ cypress/integration/editData.js
@@ -49,4 +49,29 @@ describe( 'Adds test blocks, inputs data, and verifies that the data match acros
         cy.get( '.editor-post-publish-button' ).click()
     } )
 
+    it ( 'Attempts to resolve the block issues and then edit the block using block text fields', function() {
+        cy.visit( baseURL + '/wp-admin/edit.php?post_type=page' )
+        cy.get( '[aria-label="“' + title1 + '” (Edit)"]' ).click()
+        cy.get( '[aria-label="Disable tips"]' ).click()
+        cy.get( 'button' ).contains( 'Resolve' ).click()
+        cy.get( 'button' ).contains( 'Convert to Blocks' ).click()
+        cy.get( '.statistic textarea.value').clear().type(value1).blur()
+        cy.wait(1000)
+        cy.get( '.editor-post-publish-panel__toggle' ).click()
+        cy.wait(1000)
+        cy.get( '.editor-post-publish-button' ).click()
+    } )
+
+    it ( 'Attempts to edit the "resolved" block using HTML', function() {
+        cy.visit( baseURL + '/wp-admin/edit.php?post_type=page' )
+        cy.get( '[aria-label="“' + title1 + '” (Edit)"]' ).click()
+        cy.get( '[aria-label="Disable tips"]' ).click()
+        cy.get( 'button' ).contains( 'Resolve' ).click()
+        cy.get( 'button' ).contains( 'Convert to Blocks' ).click()
+        cy.get( '.wp-block-html textarea' ).clear().type( '<div class="wp-block-cgb-block-gutenberg-simple-statistics"><div class="container"><div class="statistic"><div><div class="simple-statistic-countup" data-value="' + value1 + '" id="countup-27450">0</div></div><div class="label">' + value2 +'</div></div></div></div>' ).blur()
+        cy.wait(1000)
+        cy.get( '.editor-post-preview' ).click()
+        cy.get( '.simple-statistic-countup-counted' ).should( 'have.attr', 'data-value', value1)
+    } )
+
 } )
